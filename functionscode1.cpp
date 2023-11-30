97145e2f9315c35a53a83211d95ae18c0909625e
Karnam Nithin <98379951+nithinkarnam@users.noreply.github.com>
2023-11-30 09:47:49 +0530

    Create functionscode1.cpp

---
+++ functionscode1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void function()
+{
+  cout<<"Hello World";
+}
+int main()
+{
+ function();
+  function(); //reusability of the code is the best use of functions
+return 0;
+}
